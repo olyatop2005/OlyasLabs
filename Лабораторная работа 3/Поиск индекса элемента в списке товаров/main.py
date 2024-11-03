341d03ddacececbb719f9764df056acd8cbb044e
Оля <shlepova05@mail.ru>
2024-11-04 02:06:55 +0300

    lab 3

--- Лабораторная работа 3/Поиск индекса элемента в списке товаров/main.py
+++ Лабораторная работа 3/Поиск индекса элемента в списке товаров/main.py
@@ -1,18 +1,14 @@
-# Функция для поиска первого индекса товара
 def find_first_index(items_list, item):
     try:
-        # Вернуть индекс первого вхождения товара
         return items_list.index(item)
     except ValueError:
-        # Вернуть None, если товар не найден
         return None
 
-# Список товаров
+
 items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
 
-# Поиск индексов для заданных товаров
 for find_item in ['банан', 'груша', 'персик']:
-    index_item = find_first_index(items_list, find_item)  # Вызов функции для получения индекса товара
+    index_item = find_first_index(items_list, find_item)
     if index_item is not None:
         print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
     else:
